84d00d9313ddacef4f6a2b6cda59022773f764f7
AmeyKhale <ameykhale@gmail.com>
2018-10-27 07:09:14 +0530

    Now sends character description as well.

--- src/main/java/JavaTestScraperBot.java
+++ src/main/java/JavaTestScraperBot.java
@@ -1,4 +1,5 @@
 import io.github.cdimascio.dotenv.Dotenv;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
